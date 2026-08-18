341fe4b3870ab4281aaf8b211422bc8cc158870d
Ayushxcodes <krayashford989@gmail.com>
2026-08-18 12:43:25 +0530

    aws_chnages

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,10 +1,10 @@
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const region = process.env.AWS_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const bucketName = process.env.AWS_S3_BUCKET_NAME;
+const region = process.env.APP_AWS_REGION || process.env.AWS_REGION;
+const accessKeyId = process.env.APP_AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.APP_AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.APP_AWS_S3_BUCKET_NAME || process.env.S3_BUCKET_NAME || process.env.AWS_S3_BUCKET_NAME;
 
 // Only initialize S3 client if credentials are provided
 const isS3Configured = !!(region && accessKeyId && secretAccessKey && bucketName);
